af8e23292d5e4d744ed93b52d5cfb19b98c4cdca
joemphilips <joemphilips@gmail.com>
2019-02-18 17:24:09 +0900

    separate UnitTest and integration tests

---
+++ test-integration.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+readonly VERSION="1.0"
+if [[ "$(uname)" == 'Darwin' ]]; then
+  readonly SCRIPT_DIR_PATH=$(dirname $(greadlink -f $0))
+else
+  readonly SCRIPT_DIR_PATH=$(dirname $(readlink -f $0))
+fi
+
+cd $SCRIPT_DIR_PATH/tests/DenryuRebalancer.IntegrationTests
+docker-compose up -d
+dotnet restore &&
+  dotnet build &&
+  dotnet test
+docker-compose down
